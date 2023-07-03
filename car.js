c7572dbd7b35994f9851a2a02c27db5706289aaf
mgDentist <gmi1987@gmail.com>
2023-07-03 16:57:44 +0400

    home work 2: let and const

---
+++ car.js
@@ -0,0 +1,22 @@
+const carName = 'Carvette';
+let maxCarSpeed = 180 + ' km/h';
+let carOwner = 'no owner';
+
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
+
+carOwner = 'Jose';
+console.log('car\'s owner has been changed: ', carOwner);
+maxCarSpeed = 220 + ' km/h';
+console.log('car\'s maximum speed has been changed: ', maxCarSpeed);
+
+carOwner = 'Ignasio';
+console.log('car\'s owner has been changed: ', carOwner);
+maxCarSpeed = 80 + ' km/h';
+console.log('car\'s maximum speed has been changed: ', maxCarSpeed);
+
+carOwner = 'Loco';
+console.log('car\'s owner has been changed: ', carOwner);
+maxCarSpeed = 165 + ' km/h';
+console.log('car\'s maximum speed has been changed: ', maxCarSpeed);
---
+++ index.js
@@ -0,0 +1,18 @@
+// JavaScript-код с созданием переменных
+
+/* 
+Автор:
+Максим Григорьев
+*/
+
+const myName = 'Maksim';
+const mySurname = 'Grigorev';
+let myFavoriteDrink = 'Pure water';
+let myFavoriteAnimal = 'Dog';
+let myFavoriteProgrammingLanguage = 'JS';
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
